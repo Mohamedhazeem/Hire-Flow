1441db5ceb2a4ab641ea8526f7116f45a0733d4f
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2026-07-30 20:04:58 +0530

    fix: migration case sensitivity - use lowercase 'job' table name

--- prisma/migrations/20260730000000_add_job_slug/migration.sql
+++ prisma/migrations/20260730000000_add_job_slug/migration.sql
@@ -5,5 +5,5 @@
   - A unique constraint covering the columns `[slug]` on the table `Job` will be added. If there are existing duplicate values, this will fail.
 */
 -- AlterTable
-ALTER TABLE "public"."Job" ADD COLUMN "slug" TEXT;
-CREATE UNIQUE INDEX "Job_slug_key" ON "public"."Job"("slug");
+ALTER TABLE "public"."job" ADD COLUMN "slug" TEXT;
+CREATE UNIQUE INDEX "Job_slug_key" ON "public"."job"("slug");
